5ca0634b40a1c7bab7930b5f993d0e491c58e560
Jasm Sison <jasm.sison@gmail.com>
2016-06-24 16:45:43 +0200

    Added first xtext grammar for java 1.5 with recursion issues

---
+++ org.j2sw.parent/org.j2sw.web/src/main/webapp/xtext-resources/generated/mode-java.js
@@ -0,0 +1,35 @@
+define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
+	var HighlightRules = function() {
+		var keywords = "0|1|B|D|H|abstract|assert|b|boolean|break|byte|case|catch|char|class|continue|d|default|do|double|else|enum|extends|false|final|finally|float|for|h|if|implements|import|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|true|try|void|volatile|while";
+		this.$rules = {
+			"start": [
+				{token: "comment", regex: "\\/\\/.*$"},
+				{token: "comment", regex: "\\/\\*", next : "comment"},
+				{token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
+				{token: "string", regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"},
+				{token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
+				{token: "lparen", regex: "[\\[({]"},
+				{token: "rparen", regex: "[\\])}]"},
+				{token: "keyword", regex: "\\b(?:" + keywords + ")\\b"}
+			],
+			"comment": [
+				{token: "comment", regex: ".*?\\*\\/", next : "start"},
+				{token: "comment", regex: ".+"}
+			]
+		};
+	};
+	oop.inherits(HighlightRules, mTextHighlightRules.TextHighlightRules);
+	
+	var Mode = function() {
+		this.HighlightRules = HighlightRules;
+	};
+	oop.inherits(Mode, mText.Mode);
+	Mode.prototype.$id = "xtext/java";
+	Mode.prototype.getCompletions = function(state, session, pos, prefix) {
+		return [];
+	}
+	
+	return {
+		Mode: Mode
+	};
+});
